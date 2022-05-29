062073176ff6f9071ca90b07522703991d9d51b9
Jamim Mehdi <jamimmehdi@gmail.com>
2022-05-29 19:01:51 +0530

    added: delete operation

--- routes/aliens.js
+++ routes/aliens.js
@@ -13,17 +13,6 @@ router.get('/', async(req, res) => {
     }
 })
 
-//GET by ID request
-router.get('/:id', async(req, res) => {
-    try {
-        const aliens = await Aliens.findById(req.params.id)
-        res.json(aliens)
-    } catch(err) {
-        res.send('Error' + err)
-    }
-})
-
-
 //POST request
 router.post('/', async(req, res) => {
     const alien = new Aliens ({
@@ -35,6 +24,43 @@ router.post('/', async(req, res) => {
     try {
         let data1 = await alien.save()
         res.send(data1)
+    } catch (err) {
+        res.send('Error: ' + err)
+    }
+})
+
+//GET by ID request
+router.get('/:id', async(req, res) => {
+    try {
+        const aliens = await Aliens.findById(req.params.id)
+        res.json(aliens)
+    } catch(err) {
+        res.send('Error' + err)
+    }
+})
+
+//PATCH by ID request
+router.patch('/:id', async(req, res) => {
+    try {
+        const alien = await Aliens.findById(req.params.id)
+        alien.sub = req.body.sub
+        const updatedData = await alien.save()
+        res.send(updatedData)
+    } catch(err) {
+        res.send('Error: ' + err)
+    }
+})
+
+//DELETE by ID request
+router.delete('/:id', (req, res) => {
+    try {
+        Aliens.findByIdAndDelete(req.params.id).then(data => {
+            if(!data) {
+                res.send('id not found')
+            } else {
+                res.send('deleted')
+            }
+        })
     } catch (err) {
         res.send('Error: ' + err)
     }
